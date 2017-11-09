fad901cdb0991aa517a4fb30e1fa0279edc51f98
Akshit Sahani <akshit_sahani@hotmail.com>
2017-11-09 17:09:14 -0500

    tracks artists and albums showing up acc to designs. Need to add data attribute values within the created content

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,13 @@ class SearchController < ApplicationController
 	layout 'wider_page'
 
 	def index
-
+    if request.xhr?
+      respond_to do |format|
+        format.html
+        # format.json { render json: @resource }
+        render :layout => false
+      end
+    end
 	end
 
 end
